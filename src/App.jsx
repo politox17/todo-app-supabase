bf9a4b43b13a2edc9eac46d6e55fbe382647e9d6
politox17 <politoandrea74@gmail.com>
2025-09-27 09:13:18 +0200

    Aggiunta funzione myButton per dividere in componenti riutilizzabili

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,27 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+
+function myButton() {
+  return(
+    <>
+    <button>Visualizza</button>
+    <button>Rimuovi</button>
+    <button>Modifica</button>
+    <button>Aggiungi</button>
+    
+    </>
+  )
+}
+
 function App() {
-  const [count, setCount] = useState(0)
+ 
 
   return (
     <>
       <h1>Benvenuto!</h1>
       <h3>Scegli cosa fare</h3>
-      <button>Visualizza</button>
-      <button>Rimuovi</button>
-      <button>Modifica</button>
-      <button>Aggiungi</button>
+      <myButton />
     </>
   )
 }
