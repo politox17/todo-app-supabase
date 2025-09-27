5ae410c58d7f662a31a16e2162c7bed4b161502a
politox17 <politoandrea74@gmail.com>
2025-09-27 09:49:57 +0200

    Aggiunta la tabella delle task e fatti alcuni aggiustamenti

--- src/App.jsx
+++ src/App.jsx
@@ -3,29 +3,49 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+function showList() {
+  return (
+    <table>
+      <tr>
+        <th>Nome task</th>
+        <th>Da svolgere entro</th>
+      </tr>
+    </table>
+  )
+}
 
-function myButton() {
+
+ function MyButton({onClick, children}) {
   return(
     <>
-    <button>Visualizza</button>
-    <button>Rimuovi</button>
-    <button>Modifica</button>
-    <button>Aggiungi</button>
-    
+        <button onClick={onClick}>{children}</button>
+        
     </>
   )
 }
 
-function App() {
+function Toolbar({onShowTask}) {
+  return (
+    <>
+       <MyButton onClick={onShowTask}>Visualizza</MyButton>
+       <MyButton>Elimina</MyButton>
+       <MyButton>Modifica</MyButton>
+       <MyButton>Aggiungi</MyButton>
+    </>
+  )
+}
+
+export default function App() {
  
 
   return (
     <>
       <h1>Benvenuto!</h1>
       <h3>Scegli cosa fare</h3>
-      <myButton />
+      <Toolbar onShowTask={() => showList()} />
+      
     </>
   )
 }
 
-export default App
+
