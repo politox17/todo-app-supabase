0902c28c3c613d8d2434e7f38a0e6fdb9a9f4679
politox17 <politoandrea74@gmail.com>
2025-09-28 15:32:57 +0200

    Aggiunta logica per modificare task

--- src/App.jsx
+++ src/App.jsx
@@ -21,6 +21,29 @@ function ShowList({ tasks }) {
     </table>
   )
 }
+function EditTask({onEdit}) {
+  const [inputToEdit, setInputEdit] = useState('')
+
+  const handleSubmit = (e) => {
+   e.preventDefault();
+   if (inputToEdit.trim() !== '') {
+    onEdit(Number(inputToEdit) - 1) // uso -1 perché l'indice parte da 0, cmq fa pasring dell'input
+    setInputEdit('')
+    }
+  }
+  return (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="task">Inserisci ID task da modificare: </label>
+      <input
+        id="task"
+        type="number"
+        value={inputToEdit}
+        onChange={(e) => setInputEdit(e.target.value)}
+      />
+      <button type="submit">Modifica</button>
+    </form>
+  )
+}
 
 function DeleteTask({ onDel }) {
   const [inputToDel, setInputToDel] = useState('')
@@ -28,7 +51,7 @@ function DeleteTask({ onDel }) {
   const handleSubmit = (e) => {
     e.preventDefault()
     if (inputToDel.trim() !== '') {
-      onDel(Number(inputToDel) - 1) // uso -1 perché l'indice parte da 0
+      onDel(Number(inputToDel) - 1) // uso -1 perché l'indice parte da 0, cmq fa pasring dell'input
       setInputToDel('')
     }
   }
@@ -76,12 +99,12 @@ function MyButton({ onClick, children }) {
   return <button onClick={onClick}>{children}</button>
 }
 
-function Toolbar({ onShowTask, onAddView, onDelete }) {
+function Toolbar({ onShowTask, onAddView, onDelete, onEditing }) {
   return (
     <div>
       <MyButton onClick={onShowTask}>Visualizza</MyButton>
       <MyButton onClick={onDelete}>Elimina</MyButton>
-      <MyButton>Modifica</MyButton>
+      <MyButton onClick={onEditing}>Modifica</MyButton>
       <MyButton onClick={onAddView}>Aggiungi</MyButton>
     </div>
   )
@@ -101,6 +124,11 @@ export default function App() {
     setCurrentView('list')
   }
 
+  const handleEditTask = (indexToEdit) => {
+    setTasks(tasks.filter((_, i) => i !== indexToEdit))
+    setCurrentView('list')
+  }
+
   return (
     <>
       <h1>Benvenuto!</h1>
@@ -110,11 +138,13 @@ export default function App() {
         onShowTask={() => setCurrentView('list')}
         onAddView={() => setCurrentView('add')}
         onDelete={() => setCurrentView('delete')}
+        onEditing={() => setCurrentView('edit')}
       />
 
       {currentView === 'list' && <ShowList tasks={tasks} />}
       {currentView === 'add' && <AddTask onAdd={handleAddTask} />}
       {currentView === 'delete' && <DeleteTask onDel={handleDeleteTask} />}
+      {currentView === 'edit' && <EditTask onEdit={handleEditTask} />}
     </>
   )
 }
